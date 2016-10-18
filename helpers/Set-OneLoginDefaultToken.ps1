b6c9710550dc00e5a700dbde10903cf03ad15d64
Matt McNabb <mmcnabb@outlook.com>
2016-10-18 11:11:13 -0400

    fix #28 - get module name via PSCmdlet

--- helpers/Set-OneLoginDefaultToken.ps1
+++ helpers/Set-OneLoginDefaultToken.ps1
@@ -6,7 +6,7 @@ function Set-OneLoginDefaultToken
         $Token
     )
 
-    $ModuleName = (Get-Item -Path $PSScriptRoot).Parent.Name
+    $ModuleName = $PSCmdlet.MyInvocation.MyCommand.Module.Name
     $Module = Get-Module -Name $ModuleName
     $Commands = $Module.ExportedCommands.GetEnumerator()  | Select-Object -ExpandProperty value | Select-Object -ExpandProperty name
     foreach ($Command in $Commands)
