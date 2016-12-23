1dfb803724efe2853847ae5058fb8a84a336cda1
Matt McNabb <mmcnabb@outlook.com>
2016-12-22 23:06:47 -0500

    Updated tests

--- OneLogin.tests.ps1
+++ OneLogin.tests.ps1
@@ -6,61 +6,61 @@ if (Get-Module -Name $ModuleName) { Remove-Module $ModuleName -Force }
 Import-Module $ManifestPath
 
 # test the module manifest - exports the right functions, processes the right formats, and is generally correct
-Describe "Manifest" {
+Pester\Describe "Manifest" {
     
     $ManifestHash = Invoke-Expression (Get-Content $ManifestPath -Raw)
 
-    It "has a valid manifest" {
+    Pester\It "has a valid manifest" {
         {
             $null = Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop -WarningAction SilentlyContinue
-        } | Should Not Throw
+        } | Pester\Should Not Throw
     }
 
-    It "has a valid root module" {
-        $ManifestHash.NestedModules | Should Be "$ModuleName.psm1"
+    Pester\It "has a valid root module" {
+        $ManifestHash.NestedModules | Pester\Should Be "$ModuleName.psm1"
     }
 
-    It "has a valid Description" {
-        $ManifestHash.Description | Should Not BeNullOrEmpty
+    Pester\It "has a valid Description" {
+        $ManifestHash.Description | Pester\Should Not BeNullOrEmpty
     }
 
-    It "has a valid guid" {
-        $ManifestHash.Guid | Should Be "87e0e33a-1747-4ff2-a812-890565b4f0d1"
+    Pester\It "has a valid guid" {
+        $ManifestHash.Guid | Pester\Should Be "87e0e33a-1747-4ff2-a812-890565b4f0d1"
     }
 
-    It "has a valid version" {
-        $ManifestHash.ModuleVersion -as [Version] | Should Not BeNullOrEmpty
+    Pester\It "has a valid version" {
+        $ManifestHash.ModuleVersion -as [Version] | Pester\Should Not BeNullOrEmpty
     }
 
-    It "has a valid copyright" {
-        $ManifestHash.CopyRight | Should Not BeNullOrEmpty
+    Pester\It "has a valid copyright" {
+        $ManifestHash.CopyRight | Pester\Should Not BeNullOrEmpty
     }
 
-    It 'processes all existing format files' {
+    Pester\It 'processes all existing format files' {
         $FormatFiles = Get-ChildItem "$ModulePath\formats\" -Filter *.ps1xml | Select-Object -ExpandProperty fullname
         $ExportedFormats = $ManifestHash.FormatsToProcess | foreach {"$ModulePath\$_"}
-        $ExportedFormats | Should Be $FormatFiles
+        $ExportedFormats | Pester\Should Be $FormatFiles
     }
     
-    It 'has a valid license Uri' {
-        $ManifestHash.PrivateData.Values.LicenseUri | Should Be 'http://opensource.org/licenses/MIT'
+    Pester\It 'has a valid license Uri' {
+        $ManifestHash.PrivateData.Values.LicenseUri | Pester\Should Be 'http://opensource.org/licenses/MIT'
     }
     
-    It 'has a valid project Uri' {
-        $ManifestHash.PrivateData.Values.ProjectUri | Should Be 'https://github.com/mattmcnabb/OneLogin'
+    Pester\It 'has a valid project Uri' {
+        $ManifestHash.PrivateData.Values.ProjectUri | Pester\Should Be 'https://github.com/mattmcnabb/OneLogin'
     }
 
-    It "gallery tags don't contain spaces" {
+    Pester\It "gallery tags don't contain spaces" {
         foreach ($Tag in $ManifestHash.PrivateData.Values.tags)
         {
-            $Tag | Should Not Match '\s'
+            $Tag | Pester\Should Not Match '\s'
         }
     }
 }
 
 Describe "Functions" {
     InModuleScope -ModuleName $ModuleName {
-        Context "Get-BoundParameter" {
+        Pester\Context "Get-BoundParameter" {
             $BoundParameters = @{
                 Param1              = "Value1"
                 Param2              = "Value2"
@@ -92,16 +92,34 @@ Describe "Functions" {
                 "PipelineVariable"
             )
 
-            It "removes common parameters" {
+            Pester\It "removes common parameters" {
                 (Get-BoundParameter -BoundParameters $BoundParameters).GetEnumerator() | Where-Object {
                     $_.Name -in $Common
-                }  | Should Be $null
+                }  | Pester\Should Be $null
             }
 
-            It "removes excluded parameters" {
+            Pester\It "removes excluded parameters" {
                 $Bound = (Get-BoundParameter -BoundParameters $BoundParameters -ExcludedParameters "Param2").GetEnumerator()
-                $Bound.Name | Should Be "Param1"
+                $Bound.Name | Pester\Should Be "Param1"
 
+            }
+        }
+
+        Pester\Context "Get-OneLoginEvent" {
+            # make sure you can pass multiple filter properties
+            Pester\It "accepts multiple filter properties" {
+                Pester\Mock -CommandName Invoke-RestMethod -MockWith {[PSCustomObject]@{}}
+                $Token = [OneLogin.Token]::new()
+                { Get-OneLoginEvent -Filter @{user_id = "123456"; directory_id = "56789" } -Token $Token} | Pester\Should Not Throw
+            }
+        }
+
+        Pester\Context "Get-OneLoginUser" {
+            # make sure you can pass multiple filter properties
+            Pester\It "accepts multiple filter properties" {
+                Pester\Mock -CommandName Invoke-RestMethod -MockWith {[PSCustomObject]@{}}
+                $Token = [OneLogin.Token]::new()
+                { Get-OneLoginUser -Filter @{firstname = "Matt"; email = "matt@domain.com" } -Token $Token} | Pester\Should Not Throw
             }
         }
     }
