7a31851bb54598c7eb595419987474abdffe5648
Matt McNabb <mmcnabb@outlook.com>
2017-05-29 23:47:36 -0400

    Fix #37 - don't set default values for *Token commands

--- OneLogin/helpers/Set-OneLoginDefaultToken.ps1
+++ OneLogin/helpers/Set-OneLoginDefaultToken.ps1
@@ -8,7 +8,11 @@ function Set-OneLoginDefaultToken
 
     $ModuleName = $PSCmdlet.MyInvocation.MyCommand.Module.Name
     $Module = Get-Module -Name $ModuleName
-    $Commands = $Module.ExportedCommands.GetEnumerator()  | Select-Object -ExpandProperty value | Select-Object -ExpandProperty name
+    $Commands = $Module.ExportedCommands.GetEnumerator() |
+        Select-Object -ExpandProperty value |
+        Where-Object Name -NotLike '*Token' |
+        Select-Object -ExpandProperty name
+
     foreach ($Command in $Commands)
     {
         $Global:PSDefaultParameterValues["$Command`:Token"] = $Token
