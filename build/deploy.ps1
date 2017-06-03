95eea08007f1897573d61b06c50ec0a4445d767a
Matt McNabb <mmcnabb@outlook.com>
2017-06-02 23:30:27 -0400

    Add -verbose to deployment for logging [skip ci]

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -2,6 +2,6 @@ if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "^!Deploy")
 {
     $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
     Import-Module PowerShellGet -Force
-    Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
+    Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false -Verbose
 }
 
