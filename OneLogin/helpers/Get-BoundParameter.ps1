5fe3456ff5a2af7b153cd7675d0797a9626a10f4
Matt McNabb <mmcnabb@outlook.com>
2017-06-05 15:24:14 -0400

    format Get-BoundParameter

--- OneLogin/helpers/Get-BoundParameter.ps1
+++ OneLogin/helpers/Get-BoundParameter.ps1
@@ -16,10 +16,12 @@ function Get-BoundParameter
         $ExcludedParameters
     )
 
-    $CommonParameters = $MyInvocation.MyCommand.ParameterSets.Parameters | Where-Object Position -eq -2147483648 | Select-Object -ExpandProperty Name
+    $CommonParameters = $MyInvocation.MyCommand.ParameterSets.Parameters |
+        Where-Object Position -eq -2147483648 |
+        Select-Object -ExpandProperty Name
     $ToExclude = $CommonParameters + $ExcludedParameters
     $Hash = @{}
-    $BoundParameters.GetEnumerator() | Foreach {
+    $BoundParameters.GetEnumerator() | ForEach-Object {
         if ($_.Name -notin $ToExclude) { $Hash.Add($_.Name, $_.Value) }
     }
 
